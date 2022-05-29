2f6855b3161c10ee79469d146de7d216a2d01957
Allister Sascha <101225242+allistersascha@users.noreply.github.com>
2022-05-28 19:37:27 -0500

    Create alsasnewbizobject.js
    My new business object for the API! Can facilitate more of these too :)

---
+++ alsasnewbizobject.js
@@ -0,0 +1,9 @@
+/*
+const example = new Business(name , website, image, score)
+//name = string
+//website = string
+//image = string or path
+//score = integer (1-10)
+*/
+
+const Starbucks = new Business ('Starbucks Inc.', 'https://www.starbucks.com', 'starbuckslogo.png', 3)
