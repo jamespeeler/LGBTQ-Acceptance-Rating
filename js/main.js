980aaff7285e210c2f9f4a430ed1730b9d51650a
Samantha Mcmillion <sammy.mcmillion@gmail.com>
2022-05-30 19:43:20 -0700

    add 1-10 number generator into main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 document.querySelector('#clickMe').addEventListener('click', makeReq)
 
+let randomRating = Math.floor(Math.random()* 10)+ 1;
+console.log(Math.floor(Math.random()* 10)+ 1);
+
 async function makeReq() {
   //User input value.
   const businessName = document.querySelector("#business").value;
