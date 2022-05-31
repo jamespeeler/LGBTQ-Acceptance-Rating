980d1bb6644ab7deb1d27b9c532821bc31369e49
Michael Joel <michaeljoelt@gmail.com>
2022-05-30 19:32:04 -0700

    correcting some merge conflicts

--- server.js
+++ server.js
@@ -1,20 +1,9 @@
-class Business {
-  constructor(name, url, img, rating) {
-    this.name = name
-    this.url = url
-    this.img = img
-    this.rating = rating
-  }
-}
-
 const http = require('http');
 const fs = require('fs')
 const url = require('url');
 const querystring = require('querystring');
 const figlet = require('figlet')
 const fetch = require('node-fetch');
-
-
 
 //This is necessary for API key privacy, the module which lets server.js talk with .env
 require('dotenv').config()
@@ -26,11 +15,6 @@ const app = express()
 
 //This is where the responses from the Yelp Fusion API will be stored. It will always be an array of 5 objects: play with this if you want to play with the API results
 let apiResponse;
-
-
-
-const businessList = [];
-
 
 const server = http.createServer((req, res) => {
   const page = url.parse(req.url).pathname;
@@ -47,17 +31,16 @@ const server = http.createServer((req, res) => {
       res.end();
     });
   } else if (page == '/api') { //If API is called? Current scenario is when button is pressed.
-
-    if ('business' in params) { //Check to see if the API calls includes business, ex: `/api?business=${businessName}` I assume at least.
-      const input = params['business'].toLowerCase().replace("%20", " "); //user input //toLower and replace makes it case-insensitive and fixes spacing.
+    /*Check to see if the API calls includes business, ex: `/api?business=${businessName}`*/
+    if ('business' in params) {
+      /* handle user input */
+      const input = params['business'].toLowerCase().replace("%20", " "); //toLower and replace makes it case-insensitive and fixes spacing.
       let location = params['location'].toLowerCase().replace("%20", " ");
 
       if (location.trim() == "") {
         location = "nyc"
       }
-
-
-      //This is where the Yelp Fusion API is fetched
+      //START - Yelp Fusion API handling 
       fetch(`https://api.yelp.com/v3/businesses/search?term=${input}&location=${location}&limit=5`, {
           headers: { //These are HTTP headers, apparently important when using APIs
             'Authorization': `Bearer ${process.env.API_KEY}` //API key is hidden in the .env file || BYOA - bring your own API Key
@@ -69,46 +52,16 @@ const server = http.createServer((req, res) => {
           console.log(apiResponse); //you can see what you just received in the terminal
           res.end(JSON.stringify(apiResponse));
         });
-
-
-      // Yelp Fusion API call ends here ^^^
-
-
-      // if (input == 'bowtie behavior') {
-      //   res.writeHead(200, {
-      //     'Content-Type': 'application/json'
-      //   });
-      //   const objToJson = new Business("Bowtie Behavior", "https://www.bowtiebehavior.com/", "https://static.wixstatic.com/media/f25e9b_c67af0e6cb48435abbbf4958f86905fa~mv2.jpg/v1/fill/w_548,h_604,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/f25e9b_c67af0e6cb48435abbbf4958f86905fa~mv2.jpg", 10)
-      //   res.end(JSON.stringify(objToJson));
-      // } else if (businessList.some(busi => busi.name === input)) { //If the business is in the list.
-      //   res.writeHead(200, {
-      //     'Content-Type': 'application/json'
-      //   });
-      //   //Find the index of where it is in the list
-      //   const index = businessList.findIndex(object => {
-      //     return object.name === input;
-      //   });
-      //   //Return it as JSON data
-      //   const objToJson = businessList[index];
-      //   res.end(JSON.stringify(objToJson));
-      // } else if (params['business'] != 'leon') { //If it doesn't exist make a new business
-      //   res.writeHead(200, {
-      //     'Content-Type': 'application/json'
-      //   });
-      //   const objToJson = new Business(params['business'].toLowerCase().replace("%20", " "), "/", "/", null)
-      //   businessList.push(objToJson);
-      //   res.end(JSON.stringify(objToJson));
-      // }
+      //END - Yelp Fusion API handling
     }
 
-
-  } //else if
-  else if (page == 'css/style.css') {
+  } else if (page == '/css/style.css') {
     fs.readFile('css/style.css', function (err, data) {
       res.write(data);
       res.end();
     });
-  } else if (page == 'js/main.js') {
+
+  } else if (page == '/js/main.js') {
     fs.readFile('js/main.js', function (err, data) {
       res.writeHead(200, {
         'Content-Type': 'text/javascript'
@@ -128,7 +81,6 @@ const server = http.createServer((req, res) => {
     });
   }
 });
-
 
 
 //These 'app' code blocks are used to get CORS running on our server
